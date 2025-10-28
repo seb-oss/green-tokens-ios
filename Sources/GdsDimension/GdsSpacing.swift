a91eb1627ba5457fc188a9fb53082bfe8c58dfbc
Mayur Deshmukh <mayur.ios.dev@gmail.com>
2025-10-28 21:29:54 +0100

    Update spacing constants in GdsSpacing.swift
    Redefined CGFloat spacing constants to use a more granular and consistent scale, replacing previous large values with smaller, sequential increments. This improves clarity and usability of spacing values throughout the codebase.

--- Sources/GdsDimension/GdsSpacing.swift
+++ Sources/GdsDimension/GdsSpacing.swift
@@ -17,23 +17,23 @@
 import Foundation
 
 public extension CGFloat {
-    static let space0 = CGFloat(0.00)
-    static let space2xl = CGFloat(512.00)
-    static let space2xs = CGFloat(96.00)
-    static let space3xl = CGFloat(640.00)
-    static let space3xs = CGFloat(64.00)
-    static let space4xl = CGFloat(768.00)
-    static let space4xs = CGFloat(32.00)
-    static let space5xl = CGFloat(1024.00)
-    static let space5xs = CGFloat(16.00)
-    static let space6xl = CGFloat(1280.00)
-    static let space7xl = CGFloat(1536.00)
-    static let space8xl = CGFloat(1792.00)
-    static let space9xl = CGFloat(1920.00)
-    static let spaceL = CGFloat(320.00)
-    static let spaceM = CGFloat(256.00)
-    static let spaceMax = CGFloat(15984.00)
-    static let spaceS = CGFloat(192.00)
-    static let spaceXl = CGFloat(384.00)
-    static let spaceXs = CGFloat(128.00)
+    static let space0: CGFloat   = 0.0
+    static let space5xs: CGFloat = 1.0
+    static let space4xs: CGFloat = 2.0
+    static let space3xs: CGFloat = 4.0
+    static let space2xs: CGFloat = 6.0
+    static let spaceXs: CGFloat  = 8.0
+    static let spaceS: CGFloat   = 12.0
+    static let spaceM: CGFloat   = 16.0
+    static let spaceL: CGFloat   = 20.0
+    static let spaceXl: CGFloat  = 24.0
+    static let space2xl: CGFloat = 32.0
+    static let space3xl: CGFloat = 40.0
+    static let space4xl: CGFloat = 48.0
+    static let space5xl: CGFloat = 64.0
+    static let space6xl: CGFloat = 80.0
+    static let space7xl: CGFloat = 96.0
+    static let space8xl: CGFloat = 112.0
+    static let space9xl: CGFloat = 120.0
+    static let spaceMax: CGFloat = 999.0
 }
