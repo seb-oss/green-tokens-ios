b6e835370024b2fa434678ff469e15befa35fcc0
Herman Olsson <herman.olsson@framna.com>
2026-02-27 15:19:23 +0100

    Add Level enum and EnvironmentValue

---
+++ Sources/GdsColours/Level/Level.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+public enum Level: String {
+    case level1 = "l1"
+    case level2 = "l2"
+    case level3 = "l3"
+    
+    public var above: Self {
+        switch self {
+        case .level1: return .level2
+        case .level2: return .level3
+        case .level3: return .level3
+        }
+    }
+    
+    public var below: Self {
+        switch self {
+        case .level1: return .level1
+        case .level2: return .level1
+        case .level3: return .level2
+        }
+    }
+}
+
+public extension EnvironmentValues {
+    @Entry var level: Level = .level1
+}
